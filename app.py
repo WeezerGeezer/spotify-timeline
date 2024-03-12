b3399c886f3556aea507b06e3d2ae3b800cbb070
Mitchell Carter <mitchca@umich.edu>
2024-03-12 15:38:15 -0400

    Prod settings for deployment

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def process_playlists():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run()
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
